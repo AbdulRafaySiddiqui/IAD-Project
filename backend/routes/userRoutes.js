6547044c435dbfcfb0dcc3f79ef1c2be13f22cc9
Syed Arsalan Habib <arsalanhabib13@gmail.com>
2020-12-18 00:24:12 +0500

    Added update Profile functionality

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
     authUser,
     getUserProfile,
+    updateUserProfile,
     registerUser,
 } from "../controllers/userController.js";
 import { protect } from "../middleware/authmiddleware.js";
@@ -10,6 +11,9 @@ const router = express.Router();
 
 router.route("/").post(registerUser);
 router.post("/login", authUser);
-router.route("/profile").get(protect, getUserProfile);
+router
+    .route("/profile")
+    .get(protect, getUserProfile)
+    .put(protect, updateUserProfile);
 
 export default router;
--- frontend/src/screens/RegisterScreen.js
+++ frontend/src/screens/RegisterScreen.js
@@ -16,9 +16,9 @@ const RegisterScreen = ({ location, history }) => {
 
     const dispatch = useDispatch();
 
-    const userLogin = useSelector((state) => state.userRegister);
+    const userRegister = useSelector((state) => state.userRegister);
 
-    const { loading, error, userInfo } = userLogin;
+    const { loading, error, userInfo } = userRegister;
 
     const redirect = location.search ? location.search.split("=")[1] : "/";
 
