69cbd631cb9534521f9a9250c2f631e627cf8bff
Syed Arsalan Habib <arsalanhabib13@gmail.com>
2020-12-23 18:36:00 +0500

    Delete product functionality for admins

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -23,4 +23,18 @@ const getProductbyId = asyncHandler(async (req, res) => {
     }
 });
 
-export { getProducts, getProductbyId };
+// @desc Delete a product
+// @route DELETE /api/products/:id
+// access Public/Admin
+const deleteProduct = asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    if (product) {
+        await product.remove();
+        res.json({ message: "Product removed successfully." });
+    } else {
+        res.status(404);
+        throw new Error("Product not found");
+    }
+});
+
+export { getProducts, getProductbyId, deleteProduct };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,12 +1,15 @@
 import express from "express";
 import {
+    deleteProduct,
     getProductbyId,
     getProducts,
 } from "../controllers/productController.js";
+import { protect, admin } from "../middleware/authmiddleware.js";
+
 const router = express.Router();
 
 router.route("/").get(getProducts);
 
-router.route("/:id").get(getProductbyId);
+router.route("/:id").get(getProductbyId).delete(protect, admin, deleteProduct);
 
 export default router;
