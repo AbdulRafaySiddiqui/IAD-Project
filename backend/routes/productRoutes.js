35043a225626cf2f09117cba6fc69e71ae9638d3
Syed Arsalan Habib <arsalanhabib13@gmail.com>
2020-12-24 23:12:20 +0500

    routes and functions for product create and update in backend

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,15 +1,21 @@
 import express from "express";
 import {
+    createProduct,
     deleteProduct,
     getProductbyId,
     getProducts,
+    updateProduct,
 } from "../controllers/productController.js";
 import { protect, admin } from "../middleware/authmiddleware.js";
 
 const router = express.Router();
 
-router.route("/").get(getProducts);
+router.route("/").get(getProducts).post(protect, admin, createProduct);
 
-router.route("/:id").get(getProductbyId).delete(protect, admin, deleteProduct);
+router
+    .route("/:id")
+    .get(getProductbyId)
+    .delete(protect, admin, deleteProduct)
+    .put(protect, admin, updateProduct);
 
 export default router;
