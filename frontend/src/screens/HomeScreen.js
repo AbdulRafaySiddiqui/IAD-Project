12c716499d565ff3a8558db08f949e3807328c3b
Syed Arsalan Habib <arsalanhabib13@gmail.com>
2020-12-28 18:24:19 +0500

    Custom page titles using react helmet

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { useEffect } from "react";
 import { Col, Row } from "react-bootstrap";
 import Product from "../components/Product";
@@ -7,6 +8,7 @@ import { listProducts } from "../actions/productActions";
 import Message from "../components/Message";
 import Loader from "../components/Loader";
 import Paginate from "../components/Paginate";
+import Meta from "../components/Meta";
 import ProductCarousel from "../components/ProductCarousel";
 
 const HomeScreen = ({ match }) => {
@@ -24,7 +26,14 @@ const HomeScreen = ({ match }) => {
 
     return (
         <>
-            {!keyword && <ProductCarousel />}
+            <Meta />
+            {!keyword ? (
+                <ProductCarousel />
+            ) : (
+                <Link to='/' className='btn btn-light'>
+                    Go Back
+                </Link>
+            )}
             <h1>Latest Products</h1>
             {loading ? (
                 <Loader />
